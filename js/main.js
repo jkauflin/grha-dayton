6e4d8d0c4d068a4ac306ee8f50e751717243b688
John J Kauflin <johnkauflin@gmail.com>
2020-12-20 18:35:17 -0500

    Update layout

--- js/main.js
+++ js/main.js
@@ -22,13 +22,57 @@ var main = (function () {
 
 	//=================================================================================================================
 	// Variables cached from the DOM
-	var $document = $(document);
-
+    var $document = $(document);
+    
 	//=================================================================================================================
     // Bind events
     // Auto-close the collapse menu after clicking a non-dropdown menu item (in the bootstrap nav header)
     $(".navbar-nav li a:not('.dropdown-toggle')").on('click', function () { 
         $('.navbar-collapse').collapse('hide'); 
     });
+
+
+//    $(".navbar-nav li a:not('.dropdown-toggle')")
+/*
+    $document.on("click", "#DuesTabButton", function () {
+        var $this = $(this);
+        //console.log("Click on MediaFolderLink, data-dir = " + $this.attr('data-dir'));
+        //displayThumbnails($this.attr('data-dir'));
+        $(".nav-link").find(".active").removeClass("active");
+        $('.navbar-nav a[href="#DuesPage"]').tab('show')
+        $('.navbar-nav a[href="#DuesPage"]').addClass('active');
+    });
+    $document.on("click", "#ContactsTabButton", function () {
+        var $this = $(this);
+        //console.log("Click on MediaFolderLink, data-dir = " + $this.attr('data-dir'));
+        //displayThumbnails($this.attr('data-dir'));
+        //$(".nav-link").find(".active").removeClass("active");
+        $('.navbar-nav a(".active")').removeClass("active");
+        $('.navbar-nav a[href="#ContactsPage"]').tab('show');
+        $('.navbar-nav a[href="#ContactsPage"]').addClass('active');
+    });
+
+<ul class="nav navbar-nav ml-auto">
+        <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#HomePage" role="tab">
+        <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#ContactsPage" role="tab">
+
+
+    $(".nav .nav-link").on("click", function () {
+        $(".nav").find(".active").removeClass("active");
+        $(this).addClass("active");
+    });
+
+
+    $(document).ready(function () {
+        $(document).on('click', '.nav-item a', function (e) {
+            $(this).parent().addClass('active').siblings().removeClass('active');
+        });
+    });
+
+    $(document).on('click', '.nav-list li', function (e) {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+*/
+
 
 })(); // var main = (function(){
